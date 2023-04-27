00e6befa768d6d82d7a3bc011076126cb90d4774
Beenz <CH.SABEEN@GMAIL.COM>
2023-04-26 22:39:49 -0700

    get.post added to add new notes

--- Develop/server.js
+++ Develop/server.js
@@ -29,3 +29,34 @@ app.get("/api/notes", (req, res) => {
     res.json(JSON.parse(data));
   });
 });
+
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+    //const newNote = req.body;
+    fs.readFile(`./db/db.json`, "utf8", (err, data) => {
+      if (err) console.log(err);
+      else {
+        const notesArray = JSON.parse(data);
+        console.log(notesArray);
+        notesArray.push(newNote);
+        fs.writeFile(
+          `./db/db.json`,
+          JSON.stringify(notesArray, null, 4),
+          (err) => {
+            if (err) console.log(err);
+            else {
+              console.log(`note ${newNote.text} received`);
+            }
+          }
+        );
+      }
+    });
+  }
+});
